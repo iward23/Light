674b477f974d705ccb7f8995acc595507754dcd7
Imani Ward <idward712@gmail.com>
2022-09-16 13:06:05 -0400

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by 9i on 9/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
